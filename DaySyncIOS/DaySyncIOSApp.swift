f2fab46ca29834ca4878c83bfc0237a8d6a9bac0
Nils Zentgraf <nils.zentgraf@aol.com>
2024-02-29 12:32:20 +0100

    Initial Commit

---
+++ DaySyncIOS/DaySyncIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaySyncIOSApp.swift
+//  DaySyncIOS
+//
+//  Created by Nils Zentgraf on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct DaySyncIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
